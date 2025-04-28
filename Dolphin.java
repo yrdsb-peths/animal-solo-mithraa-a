b1d967d51ce2f631ec61c00dc4a87af58058061f
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-04-28 14:04:26 -0400

    created Dolphin hero and made it move

---
+++ Dolphin.java
@@ -0,0 +1,16 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * The Dolphin, our hero.
+ * 
+ * @author Eswarathasan
+ * @version April 2025
+ */
+public class Dolphin extends Actor
+{
+    public void act()
+    {
+        // Add your action code here.
+        move(1);
+    }
+}
