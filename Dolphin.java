a0f9131bdec5d37a16f4ad4b4dce62f09945c238
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-04-29 14:04:07 -0400

    changed the outcome of controls

--- Dolphin.java
+++ Dolphin.java
@@ -11,13 +11,13 @@ public class Dolphin extends Actor
     public void act()
     {
         // Add your action code here.
-        if(Greenfoot.isKeyDown("right"))
-        {
-            move(1);
-        }
-        else if(Greenfoot.isKeyDown("left"))
+        if(Greenfoot.isKeyDown("left"))
         {
             move(-1);
+        }
+        else if(Greenfoot.isKeyDown("right"))
+        {
+            move(1);
         }
         
         // Remove fish if dolphin eats it 
