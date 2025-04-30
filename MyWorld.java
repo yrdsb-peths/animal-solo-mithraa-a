e2bc930c929bb18e7725f6c01d2ec669d6b3e6bc
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-04-30 14:03:02 -0400

    moved dolphin's location to more middle

--- MyWorld.java
+++ MyWorld.java
@@ -13,9 +13,19 @@ public class MyWorld extends World
         super(600, 400, 1);
         
         Dolphin dolphin = new Dolphin();
-        addObject(dolphin, 350, 350);
+        addObject(dolphin, 300, 200);
         
+        createFish();
+    }
+    
+    /** 
+     * Create a new fish at a random location at the top of the screen 
+     */
+    public void createFish()
+    {
         Fish fish = new Fish();
-        addObject(fish, 350, 0);
+        int x = Greenfoot.getRandomNumber(500);
+        int y = 0 ;
+        addObject(fish, x, y);
     }
 }
