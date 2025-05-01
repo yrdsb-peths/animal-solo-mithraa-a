93e7aa8196c26e7c4782bbd8c3f5bc785cf648b5
Kalkie Eswarathasan <kalkiemithraa@gmail.com>
2025-05-01 13:41:03 -0400

    adding label

--- MyWorld.java
+++ MyWorld.java
@@ -8,14 +8,32 @@ import greenfoot.*;
  */
 public class MyWorld extends World 
 {
+    /**
+     * Constructor for the objects of the class MyWorld
+     */
+    
     public MyWorld() 
     {
         super(600, 400, 1);
         
+        // Create the dolphin object 
         Dolphin dolphin = new Dolphin();
         addObject(dolphin, 300, 250);
         
+        // Create a label 
+        Label scoreLabel = new Label(0, 80);
+        addObject(scoreLabel, 40, 40);
+        
         createFish();
+    }
+    
+    /**
+     * Increase score
+     */
+    public void increaseScore()
+    {
+        score++;
+        
     }
     
     /** 
